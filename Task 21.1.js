4935e6ed7f17bc557057f342ab1499da3888067e
helloyoujed <helloyoujed@gmail.com>
2016-05-26 23:07:45 +0300

    JS_jquery

---
+++ Task 21.1.js
@@ -0,0 +1,49 @@
+﻿'use strict';
+
+
+$("body").prepend('<h1>Task 21</h1>'
+	+ '<h2>Task 1</h2>'
+	+ '<p>Use only jQuery for layout and adding styles<br>'
+	+'Create such article block<br>'
+	+ 'The layout is flexible (its width is changed according to the browser width)</p>'
+	+ '<div class="block">'
+	+ '<div class="blockborder">'
+	+ '<div class="blockheader">Header</div>'
+	+ '<div class="blockarrow"></div>'
+	+ '<div class="blocktext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 	varius interdum orci quis sodales. Integer at mi eu lectus efficitur bibendum. Aenean eleifend semper euismod. Proin nec rutrum eros. Curabitur bibendum, metus sit amet elementum eleifend, velit erat cursus magna, vel hendrerit neque odio id est. Suspendisse feugiat augue massa, id tristique mi facilisis ut. Curabitur eget odio in quam dapibus scelerisque ut quis augue.</div>'
+	+ '</div></div>'
+	);
+
+
+$('.block').css({
+	"width": "30%",
+	"border": "1px solid #00cccc",
+	"border-radius": "10px",
+	"padding": "10px",
+	"background": "#b3fff0",
+	"font-family": "sans-serif"
+});
+
+$('.blockborder').css({
+	"border": "1px solid #00cccc",
+	"background": "white",
+	"color": "#009999"
+});
+
+$('.blockheader').css({
+	"background": "#ffcc00",
+	"color": "black",
+	"font-size": "20px",
+	"padding": "10px"
+});
+
+$('.blocktext').css("padding", "10px");
+
+$('.blockarrow').css({
+	"width": "0",
+	"height": "0",
+	"border": "15px solid transparent",
+	"border-top-color": "#ffcc00",  
+	"border-bottom": "0",
+	"margin-left": "10px"
+});
---
+++ Task 21.2.js
@@ -0,0 +1,56 @@
+﻿'use strict';
+
+$('ul').css({"position": "absolute",
+	"top": "-10em",
+	"width": "120px",
+	"height": "195px",
+	"margin": "0",
+	"padding": "0",
+	"background": "#00ace6",
+	"color": "white",
+	"list-style-type": "none",
+	"text-align": "center",
+	"border-radius": "0 0 15px 15px"
+});
+
+$('li').css("padding", "7px 5px 7px 5px");
+
+$('.top').css({"border-top": "1px solid gray",
+	"border-radius": "0 0 15px 15px"
+});
+
+$('.top').hover(
+	function() {
+		$('.top').css("box-shadow", "0 0 15px rgba(0, 191, 255, 0.6)");
+	},
+	function() {
+		$('.top').css("box-shadow", "none");
+	});
+
+$('#tpmn').change( function() {
+	if ($('#tpmn').prop('checked') === true) {
+		console.log($('input').prop('checked'));
+		$('ul').animate({top: 0}, 1500);
+	} else {
+		console.log('------');
+		$('ul').animate({top: -160}, 1500);
+	}
+});
+
+$('li').hover( 
+	function() {
+		$('li:not(.top):hover').css("background", "#0099cc");
+	},
+	function() {
+		$('li').css("background", "#00ace6");
+	});
+
+$('input').css("display", "none");
+
+
+
+
+
+
+
+
